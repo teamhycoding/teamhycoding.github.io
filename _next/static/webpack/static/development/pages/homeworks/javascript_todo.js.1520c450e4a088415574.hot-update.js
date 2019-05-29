f2f45105668aa7cd12f7b73e9aaad18cd9eefc44
kentakang <kentakang@icloud.com>
2019-05-30 01:23:10 +0900

    Deploy to github pages

---
+++ _next/static/webpack/static/development/pages/homeworks/javascript_todo.js.1520c450e4a088415574.hot-update.js
@@ -0,0 +1,247 @@
+webpackHotUpdate("static\\development\\pages\\homeworks\\javascript_todo.js",{
+
+/***/ "./pages/homeworks/javascript_todo.js":
+/*!********************************************!*\
+  !*** ./pages/homeworks/javascript_todo.js ***!
+  \********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/head */ "./node_modules/next-server/dist/lib/head.js");
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var lottie_react_web__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! lottie-react-web */ "./node_modules/lottie-react-web/dist/index.js");
+/* harmony import */ var lottie_react_web__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(lottie_react_web__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _animation_Cute__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../animation/Cute */ "./animation/Cute.json");
+var _animation_Cute__WEBPACK_IMPORTED_MODULE_4___namespace = /*#__PURE__*/__webpack_require__.t(/*! ../../animation/Cute */ "./animation/Cute.json", 1);
+var _jsxFileName = "C:\\Users\\lansy\\Desktop\\Work\\hycoding\\teamhycoding.github.io\\pages\\homeworks\\javascript_todo.js";
+
+
+
+
+
+
+var Page = function Page() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    style: {
+      width: '60%',
+      height: '100%',
+      margin: 'auto',
+      border: '1px solid #e8e8e8'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_head__WEBPACK_IMPORTED_MODULE_2___default.a, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("title", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Javascript Todo Examples"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("link", {
+    href: "/static/css/fonts.css",
+    rel: "stylesheet",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("header", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, "\uCCAB \uBC88\uC9F8 \uACFC\uC81C - Javascript Todo")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "/static/images/javascript_todo.png",
+    width: "100%",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "\uB9CC\uB4E4\uAE30 \uC704\uD574 \uD544\uC694\uD55C \uC9C0\uC2DD"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    style: {
+      marginLeft: '2em'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, "\uC790\uBC14\uC2A4\uD06C\uB9BD\uD2B8 \uAE30\uCD08"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    style: {
+      marginLeft: '2em'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, "\uC790\uBC14\uC2A4\uD06C\uB9BD\uD2B8\uB97C \uD1B5\uD55C DOM \uC870\uC791"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, "\uACFC\uC81C \uB0B4\uC6A9"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    style: {
+      marginLeft: '2em'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, "\uC81C\uACF5\uB418\uB294 HTML \uC18C\uC2A4\uC5D0 \uC790\uBC14\uC2A4\uD06C\uB9BD\uD2B8\uB97C \uD1B5\uD574 \uC791\uB3D9\uD558\uB294 \uD560\uC77C \uBAA9\uB85D \uC6F9\uC0AC\uC774\uD2B8\uB97C \uB9CC\uB4DC\uB294\uAC8C \uACFC\uC81C\uC758 \uBAA9\uD45C\uC785\uB2C8\uB2E4."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: this
+  }, "\uACFC\uC81C \uAE30\uAC04"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    style: {
+      marginLeft: '2em'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25
+    },
+    __self: this
+  }, "2019\uB144 6\uC6D4 21\uC77C\uAE4C\uC9C0"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }, "\uC81C\uCD9C \uBC29\uBC95"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    style: {
+      marginLeft: '2em'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, "\uD3F4\uB354\uB97C \uC555\uCD95\uD574\uC11C ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "mailto:lansys05@icloud.com",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, "lansys05@icloud.com"), "\uC73C\uB85C \uBCF4\uB0B4\uC8FC\uC138\uC694."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, "\uACFC\uC81C \uC900\uBE44\uC790\uB8CC"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    style: {
+      marginLeft: '2em'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "/static/files/javascript_todo.zip",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30
+    },
+    __self: this
+  }, "HTML \uD30C\uC77C (\uC704 \uC0AC\uC9C4\uC758 \uB514\uC790\uC778\uC774 \uB418\uC5B4 \uC788\uB294 HTML \uD30C\uC77C\uC785\uB2C8\uB2E4.)")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }, "\uC644\uC131 \uB41C \uACFC\uC81C \uC608\uC2DC"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    style: {
+      marginLeft: '2em'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/examples/javascript_todo",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, "\uC608\uC2DC \uD398\uC774\uC9C0"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    style: {
+      borderTop: "1px solid #e8e8e8",
+      margin: '5em',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(lottie_react_web__WEBPACK_IMPORTED_MODULE_3___default.a, {
+    width: 200,
+    height: 200,
+    options: {
+      animationData: _animation_Cute__WEBPACK_IMPORTED_MODULE_4__
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }), "\u2764\uFE0F from Seoul, HyCoding.")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Page);
+
+/***/ })
+
+})
+//# sourceMappingURL=javascript_todo.js.1520c450e4a088415574.hot-update.js.map
